179e6ee6a4ea9e148ef360ee30d66f44b96654be
njuante <juan.nunez1@alu.uclm.es>
2026-01-05 18:55:54 +0100

    Refactor network status scripts and Polybar configurations for improved readability and layout consistency; adjust spacing and offsets for better visual presentation.

--- config/bspwm/scripts/target_ip_status.sh
+++ config/bspwm/scripts/target_ip_status.sh
@@ -2,7 +2,7 @@
 ip_address=$(/bin/cat ~/.config/bspwm/scripts/target_ip | awk '{print $1}')
  
 if [ $ip_address ]; then
-    echo "%{F#e51d0b}󰓾%{F#ffffff} $ip_address%{u-}"
+    echo "%{F#e51d0b}  %{F#ffffff} $ip_address%{u-}"
 else
-    echo "%{F#e51d0b}󰓾%{u-}%{F#ffffff} No target"
+    echo "%{F#e51d0b}  %{u-}%{F#ffffff} No target"
 fi
